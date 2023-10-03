bad173546df6e24582452a943ac7ad13c563cca0
Felicitas <felicitas.aguerralde@gmail.com>
2023-10-03 13:25:39 -0300

    Agrego desplegables sin darle formato

--- js/menu.js
+++ js/menu.js
@@ -2,5 +2,11 @@
 //menu responsive
 document.querySelector(".btn-menu").addEventListener("click", toggleMenu);
 function toggleMenu() {
-    document.querySelector(".barraNav").classList.toggle("show");
+    document.querySelector(".desplegable").classList.toggle("show");
+}
+
+//cruz para cerrar
+document.querySelector(".btn-cruz").addEventListener("click", toggleMenu);
+function toggleMenu() {
+    document.querySelector(".desplegable").classList.toggle("cerrar");
 }
