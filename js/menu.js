9a472d04ed0b31897cf5f28ff668e23c4678de60
Felicitas <felicitas.aguerralde@gmail.com>
2023-10-04 09:49:25 -0300

    Agrego animaciones

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,34 @@
 "use strict"
+//------------------Efecto mostrar--------------
+const mostrar= document.querySelector(".btn-menu");
+
 //menu responsive
-document.querySelector(".btn-menu").addEventListener("click", toggleMenu);
-function toggleMenu() {
-    document.querySelector(".desplegable").classList.toggle("show");
-}
+mostrar.addEventListener("click", mostrar);
+function mostrar() {
+    document.querySelector(".desplegable").classList.mostrar("show");
+};
+
+//------------------Efecto ocultar--------------
+const ocultar= document.querySelector(".btn-cruz");
 
 //cruz para cerrar
-document.querySelector(".btn-cruz").addEventListener("click", toggleMenu);
-function toggleMenu() {
-    document.querySelector(".desplegable").classList.toggle("cerrar");
-}
+ocultar.addEventListener("click", ocultar);
+function ocultar() {
+    document.querySelector(".desplegable").classList.ocultar("cerrar");
+};
+
+//------------------Efecto agrandar--------------
+const agrandar=document.querySelector(".agrandar");
+
+agrandar.addEventListener("click", ()=>{
+// Agrandar
+agrandar.style.transform = "scale(1.5)";
+
+// Espera un cuarto de segundo
+setTimeout(() => {
+  // Muestra el botón
+  agrandar.style.opacity = 1;
+  agrandar.style.transform = "scale(1)";
+}, 250);
+});
+
