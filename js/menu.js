4fe0102759bebb1988ea342b695430a662a87858
Felicitas <felicitas.aguerralde@gmail.com>
2023-10-05 21:43:14 -0300

    Animacion resgitro ok

--- js/menu.js
+++ js/menu.js
@@ -13,22 +13,90 @@ function mostrarPopup() {
   doc.classList.add("visible");
   doc.classList.add("ocultar");
   // Gira la imagen
-  const load = document.querySelector(".load");
-  const giro = setInterval(() => {
-    load.style.transform = `rotate(${Math.random() * 360}deg)`;
-  }, 600);
+  //const load = document.querySelector(".load");
+  //const giro = setInterval(() => {
+  // load.style.transform = `rotate(${Math.random() * 360}deg)`;
+  //}, 600);
 
   // Agrandar la imagen
-  load.style.transform = `scale(1.2)`;
-  load.style.transition = "transform 0.5s";
+  //load.style.transform = `scale(1.2)`;
+  //load.style.transition = "transform 0.5s";
 
-  // Redirección después de 2 segundos
+  // Porcentajes de carga
+  const porc1 = document.querySelector(".porc1");
+  const porc2 = document.querySelector(".porc2");
+  const porc3 = document.querySelector(".porc3");
+  const porc4 = document.querySelector(".porc4");
+  const porc5 = document.querySelector(".porc5");
+
+  function mostrarPorc1() {
+    porc1.style.display = "flex";
+  }
+
+  function ocultarPorc1() {
+    porc1.style.display = "none";
+  }
+
+  //Arrancan ocultas
+  setTimeout(ocultarPorc2, 0);
+  setTimeout(ocultarPorc3, 0);
+  setTimeout(ocultarPorc4, 0);
+  setTimeout(ocultarPorc5, 0);
+
+  // Establece un temporizador para mostrar la imagen
+  setTimeout(mostrarPorc1, 1599);
+  // Establece un temporizador para ocultar la imagen
+  setTimeout(ocultarPorc1, 1600);
+
+  function mostrarPorc2() {
+    porc2.style.display = "flex";
+  }
+  function ocultarPorc2() {
+    porc2.style.display = "none";
+  }
+
+  setTimeout(mostrarPorc2, 1600);
+  setTimeout(ocultarPorc2, 2500);
+
+  function mostrarPorc3() {
+    porc3.style.display = "flex";
+  }
+
+  function ocultarPorc3() {
+    porc3.style.display = "none";
+  }
+
+  setTimeout(mostrarPorc3, 2500);
+  setTimeout(ocultarPorc3, 3500);
+
+  function mostrarPorc4() {
+    porc4.style.display = "flex";
+  }
+
+  function ocultarPorc4() {
+    porc4.style.display = "none";
+  }
+
+  setTimeout(mostrarPorc4, 3500);
+  setTimeout(ocultarPorc4, 4500);
+
+  function mostrarPorc5() {
+    porc5.style.display = "flex";
+  }
+
+  function ocultarPorc5() {
+    porc5.style.display = "none";
+  }
+
+  setTimeout(mostrarPorc5, 4500);
+  setTimeout(ocultarPorc5, 5000);
+
   setTimeout(() => {
-    window.location.href = "index.html";
+    window.location.href = "formBienvenido.html";
     clearInterval(giro);
   }, 5000);
 }
-//------------------Efecto mostrar--------------
+
 const mostrar = document.querySelector(".btn-menu");
 
 //menu responsive
